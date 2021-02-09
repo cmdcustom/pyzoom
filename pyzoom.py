55bdda2e8f5fc4540fa6d71255703aa2120b7493
cmdcustom <76268123+cmdcustom@users.noreply.github.com>
2021-02-08 21:42:48 -0700

    Update pyzoom.py

--- pyzoom.py
+++ pyzoom.py
@@ -1,3 +1,17 @@
 import os, pyautogui
 userp = os.environ['USERPROFILE']
 zoomdriver = userp + r"\AppData\Roaming\Zoom\bin\Zoom.exe"
+
+def Allowed(): #check if let in meeting
+  if (pyautogui.locateOnScreen('prtc.png') is not None):
+    commandres = "acep"
+    return commandres
+  elif (pyautogui.locateOnScreen('prtc.png') is None):
+    commandres = "not yet"
+    return commandres
+  
+ 
+     
+  
+ 
+  
